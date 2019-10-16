c985e36bcddc346f4d4001bbe67878fcda98b24c
tugayakca <tugayakca96@gmail.com>
2019-10-16 15:45:42 +0300

    uppp

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Flutter Demo Homeee Page'),
     );
   }
 }
