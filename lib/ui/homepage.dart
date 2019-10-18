65f2c3a4bd1b2da0d22e963996e16d191b9451bc
Mucahit Tozal <mctozal@gmail.com>
2019-10-18 16:26:04 +0300

    adding manifest permissions, and importing the dependencies

--- lib/ui/homepage.dart
+++ lib/ui/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,10 +10,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(title: Text('Smart Ambulance'),),
-      body: Center(child: Text('cool',
-      ))
-      
-);
+        appBar: AppBar(
+          title: Text('Smart Ambulance'),
+        ),
+        body: FireMap());
+  }
+}
+
+class FireMap extends StatefulWidget {
+  @override
+  _FireMapState createState() => _FireMapState();
+}
+
+class _FireMapState extends State<FireMap> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(24.142,-110.321),
+          zoom:15
+        ),
+
+      )
+    ],);
   }
 }
