baf1c54311a4e5087e2a52a9b8417a11576d016a
Mucahit Tozal <mctozal@gmail.com>
2019-11-17 23:57:56 +0300

    locator,router

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,10 +1,18 @@
 import 'package:get_it/get_it.dart';
 import 'core/services/smartAmbulanceApi.dart';
 import 'core/viewmodels/crudModel.dart';
+import 'states/authenticationState.dart';
+import 'states/managerState.dart';
+import 'states/mapState.dart';
+import 'ui/Authentication/landingPage.dart';
 
 GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => FirebaseApi('users'));
-  locator.registerLazySingleton(() => CRUDModel()) ;
+  locator.registerLazySingleton(() => CRUDModel());
+  locator.registerLazySingleton(() => LandingPage());
+  locator.registerLazySingleton(() => MapState());
+  locator.registerLazySingleton(() => AuthenticationState());
+  locator.registerLazySingleton(() => ManagerState());
 }
--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -1,29 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:smart_ambulance/ui/Authentication/signInPage.dart';
 import 'package:smart_ambulance/ui/homepage.dart';
 
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/' :
-        return  MaterialPageRoute(
-          builder: (_)=> HomePage()
-        );
-      case '/addProduct' :
-        return MaterialPageRoute(
-          builder: (_)=> HomePage()
-        ) ;
-      case '/productDetails' :
-        return MaterialPageRoute(
-            builder: (_)=> HomePage()
-        ) ;
+      case '/home':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/map':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/settings':
+        return MaterialPageRoute(builder: (_) => HomePage());
+      case '/authentication':
+        return MaterialPageRoute(builder: (_) => SignInPage());
+      case '/manager':
+        return MaterialPageRoute(builder: (_) => HomePage());
       default:
         return MaterialPageRoute(
             builder: (_) => Scaffold(
-              body: Center(
-                child: Text('No route defined for ${settings.name}'),
-              ),
-            ));
+                  body: Center(
+                    child: Text('No route defined for ${settings.name}'),
+                  ),
+                ));
     }
   }
 }
