58d032eea0fe15e55f704722d1a1ad9605807fc0
Mucahit Tozal <mctozal@gmail.com>
2019-11-26 03:01:05 +0300

    get all distances .

--- lib/states/hospitalState.dart
+++ lib/states/hospitalState.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:smart_ambulance/model/distance.dart';
 import 'package:smart_ambulance/model/location.dart';
 import 'package:smart_ambulance/requests/google_request.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HospitalState with ChangeNotifier {
   GoogleMapsServices _googleMapsServices = GoogleMapsServices();
   List<LocationHospital> _list;
-
   List<LocationHospital> get list => _list;
+  Distance _listDistance;
+  Distance get listDistance => _listDistance;
 
   HospitalState() {
     showHospitals();
@@ -15,4 +18,9 @@ class HospitalState with ChangeNotifier {
   Future<List<LocationHospital>> showHospitals() async {
     return _list = await _googleMapsServices.getHospitals();
   }
+
+  Future<Distance> showDistance(LatLng l1,LatLng l2) async {
+    return _listDistance = await _googleMapsServices.getMatrixDistance(l1, l2);
+  }
+
 }
--- lib/ui/hospitalList.dart
+++ lib/ui/hospitalList.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:smart_ambulance/model/distance.dart';
 import 'package:smart_ambulance/states/hospitalState.dart';
 import 'package:provider/provider.dart';
+import 'package:smart_ambulance/states/mapState.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+ Distance distance;
 
 class HospitalUI extends StatelessWidget {
   @override
@@ -37,8 +42,10 @@ class _HospitalListState extends State<HospitalList> {
                       ? ListView.builder(
                           itemCount: snapshot.data.length,
                           itemBuilder: (BuildContext context, int index) {
+                            final mapState = Provider.of<MapState>(context, listen: false);
+                            getDistance(mapState.initialPosition,LatLng(snapshot.data[index].latitude,snapshot.data[index].longitude));
                             return ListTile(
-                              leading: Icon(Icons.local_hospital),
+                              leading: distance!=null ?Text(distance.duration):Text('no distance calculated'),
                               title: Text(snapshot.data[index].name.toString()),
                             );
                           },
@@ -52,4 +59,11 @@ class _HospitalListState extends State<HospitalList> {
                 }))
         : Positioned(child: Container());
   }
+
+  getDistance( LatLng source,LatLng destination ) async {
+        final hospitalState = Provider.of<HospitalState>(context,listen: false);
+    distance = await hospitalState.showDistance(source,destination);
+    return distance;
+  }
+
 }
